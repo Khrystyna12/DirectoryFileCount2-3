3d23b9c394ba49f5164602221088f3d854cd2d48
Khrystyna12 <32126443+Khrystyna12@users.noreply.github.com>
2018-12-08 21:22:36 +0200

    Add files via upload

---
+++ Tools/FileFolderHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryFileCount.Tools
+{
+    public static class FileFolderHelper
+    {
+        private static readonly string AppDataPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        internal static readonly string ClientFolderPath =
+            Path.Combine(AppDataPath, "DirectoryFileCount");
+
+        internal static readonly string LogFolderPath =
+            Path.Combine(ClientFolderPath, "Log");
+
+        internal static readonly string LogFilepath = Path.Combine(LogFolderPath,
+            "App_" + DateTime.Now.ToString("YYYY_MM_DD") + ".txt");
+
+        public static readonly string StorageFilePath =
+            Path.Combine(ClientFolderPath, "Storage.walsim");
+
+        public static readonly string LastUserFilePath =
+            Path.Combine(ClientFolderPath, "LastUser.walsim");
+
+        public static void CheckAndCreateFile(string filePath)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (!file.Directory.Exists)
+                {
+                    file.Directory.Create();
+                }
+                if (!file.Exists)
+                {
+                    file.Create().Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
